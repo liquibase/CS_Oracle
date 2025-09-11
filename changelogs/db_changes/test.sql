fe930d5080364be2ffa43e30fb4cde7d40063c13
Amy Smith <asmith@liquibase.com>
2025-09-11 17:29:19 -0500

    11

--- changelogs/db_changes/test.sql
+++ changelogs/db_changes/test.sql
@@ -11,3 +11,15 @@ create table table10 (
    CONSTRAINT "xak_table10" UNIQUE (id,name)
 )
 --rollback drop table table10
+
+--changeset adeel:table11
+create table table11 (
+   id int,
+   name varchar(50) not null,
+   address1 varchar(50),
+   address2 varchar(50),
+   city varchar(30),
+   CONSTRAINT "xpk_table11" PRIMARY KEY (id),
+   CONSTRAINT "xak_table11" UNIQUE (id,name)
+)
+--rollback drop table table11
